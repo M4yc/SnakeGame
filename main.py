883755454203163c52905d0d6144e3624bbbe0f6
Maycon Vinicius <mayconvbatista84@gmail.com>
2024-05-11 16:43:09 -0300

    :sparkles: feat: ResetGame

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ FPS = 20
 bg_inicial = pygame.image.load("assets/Tela_inicial_2.png").convert()
 bg_game = pygame.image.load("assets/Background_2.png").convert()
 icon_maca = pygame.image.load("assets/icon-maca.png").convert()
+gif_inicial = pygame.image.load("assets/Pygame.gif")
 
 def telainicial():
     intro = True
@@ -37,10 +38,22 @@ def telainicial():
                 if event.key == pygame.K_RETURN:
                     intro = False
         window.fill(black)
-        window.blit(bg_inicial, (0,0))
+        window.blit(gif_inicial, (0,0))
 
         pygame.display.update()
         clock.tick(15)
+        
+def resetGame():
+    
+    global size_snake, pixels, x, y, velocidade_x, velocidade_y
+    size_snake = 1
+    pixels = []
+    x = largura / 2
+    y = altura / 2
+    velocidade_x = 0
+    velocidade_y = 0
+    
+    apple_x, apple_y = generate_apple()
 
 def show_score(choice, color, font, size, score):
     score_font = pygame.font.SysFont(font, size)
@@ -63,8 +76,8 @@ def screen_GameOver(score):
     show_score(0, red, 'Pixelify Sans', 20, score)
     pygame.display.flip()
     time.sleep(3)
-    pygame.quit()
-    sys.exit()
+    resetGame()
+    
 
 def generate_apple():
     apple_x = round(random.randrange(0, largura - size_pixel) / float(size_pixel)) * float(size_pixel)
@@ -103,6 +116,9 @@ def select_speed(key):
     return speed_x, speed_y
 
 def play_game():
+    
+    resetGame()
+    
     gameOver = False
     x = largura/2
     y = altura/2
@@ -128,6 +144,8 @@ def play_game():
         if x < 0 or x >= largura or y < 0 or y >= altura:
             screen_GameOver(size_snake - 1)
             gameOver = True
+            telainicial()
+            play_game()
 
         x += velocidade_x
         y += velocidade_y
@@ -141,6 +159,8 @@ def play_game():
             if pixel == [x, y]:
                 screen_GameOver(size_snake - 1)
                 gameOver = True
+                telainicial()
+                play_game()
 
         design_snake(size_pixel, pixels)
         design_score(size_snake - 1)
